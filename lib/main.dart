ad9cb617f15526fd040050165418303116ddfaf6
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2021-11-23 12:45:05 +1300

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ void main() {
   return runApp(MyApp());
 }
 
-void postData() async {
+Future postData(String short) async {
   final url = "https://url-shortener.fusionsid.repl.co/api/";
-  final response = await post(Uri.parse(url), body: {"url": "instagram.com"});
+  final response = await post(Uri.parse(url), body: {"url": short});
   print(response.body);
 }
 
@@ -25,11 +25,28 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class inputWidget extends StatelessWidget {
+class inputWidget extends StatefulWidget {
+  const inputWidget({ Key? key }) : super(key: key);
+
+  @override
+  _inputWidgetState createState() => _inputWidgetState();
+}
+
+class _inputWidgetState extends State<inputWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: TextField(),
+      padding:EdgeInsets.all(10),
+      child: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(hintText: "Url you want to shorten")
+          ),
+          ElevatedButton(
+            onPressed: (){}, child: Text("Submit")
+          ),
+        ],
+      )   
     );
   }
 }
